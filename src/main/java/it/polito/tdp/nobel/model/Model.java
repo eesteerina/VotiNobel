c4019363cede6f15746df2c7563a25c33e51d6e5
esterandreetto <esterandreetto@localhost>
2023-04-03 10:14:05 +0200

    fine

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,96 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
+	
+	private List<Esame> allEsami;
+	private Set<Esame> migliore;
+	private double mediaMigliore;
+	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.allEsami = dao.getTuttiEsami();
+	}
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+		
+		migliore = new HashSet<>();  // lo inizializzo qua per essere sicuro che sia vuota, è temporanea, esiste solo quando chiamo il metodo
+		mediaMigliore = 0.0;
+		
+		Set<Esame> parziale = new HashSet<>();
+		
+		cercaMeglio(parziale, 0, numeroCrediti);
+		
+		return migliore;	
 	}
 
+	private void cercaMeglio(Set<Esame> parziale, int L, int numeroCrediti) {
+		
+		int sommaCrediti = sommaCrediti(parziale);
+		
+		if(sommaCrediti > numeroCrediti) {
+			return;
+		}
+		if(sommaCrediti == numeroCrediti) {   //qui potrei avere una soluzione
+			double mediaVoti = calcolaMedia(parziale);
+			if(mediaVoti > mediaMigliore) {
+				mediaMigliore = mediaVoti;
+				migliore = new HashSet<>(parziale);
+			}
+			return;
+		}
+		
+		if(L == allEsami.size()) {
+			return;
+		}
+		
+		// provo ad aggiungere il prossimo elemento
+		// L = 0 {e1} / { }
+		// L = 1 {e1, e2} - {e1} / {e2} - { }
+		parziale.add(allEsami.get(L));
+		cercaMeglio(parziale, L+1, numeroCrediti);
+		parziale.remove(allEsami.get(L));
+		
+		// provo a non aggiungere l'elemento
+		cercaMeglio(parziale, L+1, numeroCrediti);
+		
+	}
 	
+	private void cerca(Set<Esame> parziale, int L, int numeroCrediti) {
+		
+		int sommaCrediti = sommaCrediti(parziale);
+		
+		if(sommaCrediti > numeroCrediti) {
+			return;
+		}
+		if(sommaCrediti == numeroCrediti) {   //qui potrei avere una soluzione
+			double mediaVoti = calcolaMedia(parziale);
+			if(mediaVoti > mediaMigliore) {
+				mediaMigliore = mediaVoti;
+				migliore = new HashSet<>(parziale);
+			}
+			return;
+		}
+		
+		if(L == allEsami.size()) {
+			return;
+		}
+		
+		for(Esame e : allEsami) {
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca(parziale, L+1, numeroCrediti);
+				parziale.remove(e);
+			}
+		}
+		
+	}
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
